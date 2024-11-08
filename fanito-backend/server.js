f7f9fdbb3be02e34e73d70c9e9d5143484250bba
kandys24 <ccumbica24@gmail.com>
2024-11-08 17:51:45 +0100

    add cor changes

--- fanito-backend/server.js
+++ fanito-backend/server.js
@@ -32,6 +32,13 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://fanito.netlify.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.json());
 
 // Serve static files from the 'uploads' directory
