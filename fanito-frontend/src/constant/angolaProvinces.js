3bd9e17d78e75d99243136f385c959182497589d
kandys24 <ccumbica24@gmail.com>
2024-11-08 14:47:08 +0100

    const config

--- fanito-frontend/src/constant/angolaProvinces.js
+++ fanito-frontend/src/constant/angolaProvinces.js
@@ -1,24 +1,15 @@
 // Array of Angola provinces with IDs
 const angolaProvinces = [
-    { id: 1, name: "Bengo" },
-    { id: 2, name: "Benguela" },
-    { id: 3, name: "Bié" },
-    { id: 4, name: "Cabinda" },
-    { id: 5, name: "Cuando Cubango" },
-    { id: 6, name: "Cuanza Norte" },
-    { id: 7, name: "Cuanza Sul" },
-    { id: 8, name: "Cunene" },
-    { id: 9, name: "Huambo" },
-    { id: 10, name: "Huíla" },
-    { id: 11, name: "Luanda" },
-    { id: 12, name: "Lunda Norte" },
-    { id: 13, name: "Lunda Sul" },
-    { id: 14, name: "Malanje" },
-    { id: 15, name: "Moxico" },
-    { id: 16, name: "Namibe" },
-    { id: 17, name: "Uíge" },
-    { id: 18, name: "Harare" },
-    { id: 19, name: "Zaire" }
+    { id: 1, name: "Bulawayo" },
+    { id: 2, name: "Harare" },
+    { id: 3, name: "Manicaland" },
+    { id: 4, name: "Mashonaland Central" },
+    { id: 5, name: "Mashonaland East" },
+    { id: 6, name: "Mashonaland West" },
+    { id: 7, name: "Masvingo" },
+    { id: 8, name: "Matabeleland North" },
+    { id: 9, name: "Matabeleland South" },
+    { id: 10, name: "Midlands" }
 ];
 
 // Export the array for use in other modules
